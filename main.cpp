fcc4d02cc0e8135d8676bdb6d27ed76dc202fe9d
Aleksy Grabowski <hurufu@gmail.com>
2020-12-11 22:16:12 +0100

    Map Nexo results to main() return codes
    This was added to help other programs (like nexui) to restart nexoid when it's
    needed, based on it's return code

--- main.cpp
+++ main.cpp
@@ -11,8 +11,20 @@ extern "C" {
 
 using namespace std;
 
+static int
+map_ProcedureResultToExitCode(const enum ProcedureResult r) noexcept {
+    switch (r) {
+        case PR_TERMINATE: return 0;
+        case PR_SHUTDOWN: return 1;
+        case PR_REBOOT: return 2;
+        default:
+            break;
+    }
+    return 255;
+}
+
 int main() {
     const enum ProcedureResult pRes = Main();
     cout << __FILE__ << ':' << __LINE__ << '@' << __PRETTY_FUNCTION__ << " pRes: " << (int)pRes << ' ' << pRes << endl;
-    return 0;
+    return map_ProcedureResultToExitCode(pRes);
 }
