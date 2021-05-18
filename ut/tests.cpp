b72584a7daea374ca367722cd2ecbcdef8e6692a
Aleksy Grabowski <alg@novelpay.pl>
2021-05-18 19:15:58 +0200

    Add unit testing framework

---
+++ ut/tests.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "catch_amalgamated.hpp"
+#include "utils.hpp"
+
+template <typename T, T S> static constexpr T C = S;
+
+TEST_CASE("Integer cast", "[utils]") {
+    CHECK(integer_cast<int>(0) == 0);
+    CHECK(integer_cast<int>(C<size_t,5>) == 5);
+    CHECK(integer_cast<bool>(C<intmax_t,1>) == 1);
+    CHECK(integer_cast<bool>(C<uintmax_t,0>) == 0);
+    CHECK_THROWS(integer_cast<uint8_t>(C<int,-1>));
+    CHECK_THROWS(integer_cast<int8_t>(C<int16_t,-129>));
+}
