752502a470fa9d431c46d0219ebe2fa236084a54
Aleksy Grabowski <hurufu@gmail.com>
2021-06-13 23:43:03 +0200

    Draft of a new Randapi interface

--- randapi.cpp
+++ randapi.cpp
@@ -4,8 +4,42 @@ extern "C" {
 }
 
 #include <random>
+#include <variant>
 
 using namespace std;
+
+namespace nexoid {
+    struct Randapi {
+        typedef variant<uint8_t, TerminalErrorReason> Generate_Random_Number_Result;
+
+        virtual ~Randapi() noexcept = default;
+        virtual Generate_Random_Number_Result Generate_Random_Number(const uint8_t upperLimit) noexcept = 0;
+    };
+}
+
+class E1kdKeeper {
+public:
+    TerminalErrorReason handle_exception(const char* proc_name) {
+        return TE_HARDWARE_ERROR;
+    }
+};
+
+class StdRandapi : public nexoid::Randapi {
+    E1kdKeeper& keeper;
+public:
+    StdRandapi(E1kdKeeper& k) : keeper(k) { }
+    ~StdRandapi() = default;
+    Generate_Random_Number_Result Generate_Random_Number(const uint8_t upperLimit) noexcept override;
+};
+
+nexoid::Randapi::Generate_Random_Number_Result
+StdRandapi::Generate_Random_Number(const uint8_t upperLimit) noexcept try {
+    random_device rd;
+    uniform_int_distribution<uint8_t> dist(0, upperLimit);
+    return dist(rd);
+} catch (...) {
+    return keeper.handle_exception(__func__);
+}
 
 enum ScapiResult
 randapi_Generate_Random_Number(const uint8_t upperLimit, uint8_t* const randomNumber) noexcept try {
