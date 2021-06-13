e2de468a9d73b09e9b0b25e3d4f258cae087bd58
Aleksy Grabowski <hurufu@gmail.com>
2021-06-13 19:58:28 +0200

    Implement generation of a single random byte
    It's needed for Biased Random Selection procedure

--- randapi.cpp
+++ randapi.cpp
@@ -1,7 +1,21 @@
+extern "C" {
 #include "nexoid-ed/include/scapi.h"
+#include "nexoid-ed/include/gtd.h"
+}
+
+#include <random>
+
+using namespace std;
 
 enum ScapiResult
-randapi_Generate_Random_Number(const uint8_t upperLimit, uint8_t* randomNumber) noexcept {
+randapi_Generate_Random_Number(const uint8_t upperLimit, uint8_t* const randomNumber) noexcept try {
+    random_device rd;
+    uniform_int_distribution<uint8_t> dist(0, upperLimit);
+    *randomNumber = dist(rd);
+    return SCAPI_OK;
+} catch (...) {
+    ttd.terminalErrorReason = TE_HARDWARE_ERROR;
+    ttd.terminalErrorIndicator = true;
     return SCAPI_NOK;
 }
 
